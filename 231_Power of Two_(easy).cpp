635ba9190d3a99d4a90a4c830dfa94d3b475f703
MADHUMANTI SAMANTA <63427223+madhumanti-Gh@users.noreply.github.com>
2021-05-16 00:43:57 +0530

    Add files via upload

---
+++ 231_Power of Two_(easy).cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if( __builtin_popcountll(n) == 1 ){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
