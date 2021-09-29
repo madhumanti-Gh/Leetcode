355a889ca5760804a3b7c1e6023fe219c07fe1fe
MADHUMANTI SAMANTA <63427223+madhumanti-Gh@users.noreply.github.com>
2021-09-30 00:13:55 +0530

    Add files via upload

---
+++ 287_Find the Duplicate Number_(medium).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int mid;
+        while(low<high){
+            mid=(low+high)/2;
+            int count=0;
+            for(int num:nums){
+                if(num<=mid) count++;
+            }
+            if(count>mid) high=mid;
+            else low=mid+1;
+        }
+        return low;
+    }
+};
