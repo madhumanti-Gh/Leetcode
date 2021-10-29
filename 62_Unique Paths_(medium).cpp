8892c239fc23c7cfb0858878d5bd6279fd5915bb
MADHUMANTI SAMANTA <63427223+madhumanti-Gh@users.noreply.github.com>
2021-10-29 19:08:39 +0530

    Update 62_Unique Paths_(medium).cpp

--- 62_Unique Paths_(medium).cpp
+++ 62_Unique Paths_(medium).cpp
@@ -10,3 +10,20 @@ public:
         return dp[m-1][n-1];
     }
 };
+
+// Memoization approach
+class Solution {
+public:
+    int cache[102][102];
+    int recursion(int m, int n){
+        if( cache[m][n] != -1 )return cache[m][n];
+        if( m == 0 || n == 0 )return 1;
+        int ans = recursion( m, n-1 ) + recursion( m-1, n );
+        cache[m][n] = ans;
+        return ans;
+    }
+    int uniquePaths(int m, int n) {
+        memset( cache, -1, 102*102*sizeof(cache[0][0]) );
+        return recursion( m-1, n-1);
+    }
+};
