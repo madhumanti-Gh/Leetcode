96880161d18b0ba2f9764706f9a60e5d9744609e
MADHUMANTI SAMANTA <63427223+madhumanti-Gh@users.noreply.github.com>
2022-01-13 00:09:27 +0530

    Add files via upload

---
+++ 700_Search in a Binary Search Tree_(easy).cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if( !root )return NULL;
+        TreeNode *cur = root;
+        while( cur ){
+            if( cur->val == val )return cur;
+            if( cur->val < val )cur = cur->right;
+            else cur = cur->left;
+        }
+        return NULL;
+    }
+};
