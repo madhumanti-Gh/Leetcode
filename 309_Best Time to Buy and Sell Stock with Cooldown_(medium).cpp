8c659ff652513d985cd41beea0b356cadb284b3a
MADHUMANTI SAMANTA <63427223+madhumanti-Gh@users.noreply.github.com>
2022-01-14 00:03:58 +0530

    Update 309_Best Time to Buy and Sell Stock with Cooldown_(medium).cpp

--- 309_Best Time to Buy and Sell Stock with Cooldown_(medium).cpp
+++ 309_Best Time to Buy and Sell Stock with Cooldown_(medium).cpp
@@ -19,7 +19,7 @@ public:
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size(), i, no_stock_prev = 0, in_hand_prev = 0- prices[0], sold_prev = 0, no_stock, in_hand, sold;
+        int n = prices.size(), i, no_stock_prev = 0, in_hand_prev = 0 - prices[0], sold_prev = 0, no_stock = 0, in_hand = 0, sold = 0;
         for( i = 1; i < n; i++ ){
             no_stock = max( no_stock_prev, sold_prev );
             in_hand = max( ( no_stock_prev - prices[i] ), in_hand_prev );
