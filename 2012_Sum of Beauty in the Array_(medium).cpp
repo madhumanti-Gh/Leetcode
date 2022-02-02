560ef8a13e56bf2a346209701b14403c34b57dc8
MADHUMANTI SAMANTA <63427223+madhumanti-Gh@users.noreply.github.com>
2022-02-03 01:27:22 +0530

    Add files via upload

---
+++ 2012_Sum of Beauty in the Array_(medium).cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int sumOfBeauties(vector<int>& nums) {
+        int n = nums.size(), i, mx = nums[0], beauty = 0;
+        
+        vector<int> mins( n );
+        mins[n-1] = nums[n-1];
+        for( i = n-2; i >= 0; i-- ){
+            mins[i] = min( nums[i], mins[i+1] );
+        }
+        
+        for( i = 1; i < n-1; i++ ){
+            if( nums[i] > mx && nums[i] < mins[i+1] )beauty += 2;
+            else if( nums[i] > nums[i-1] && nums[i] < nums[i+1] )beauty++;
+            mx = max( mx, nums[i] );
+        }
+        return beauty;
+    }
+};
